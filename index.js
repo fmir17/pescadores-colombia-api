80252708c9b7a5ee8524a2d24a6ca552fef5658e
fmir17 <f.mir17@gmail.com>
2013-12-29 17:07:02 -0500

    permissions added

--- index.js
+++ index.js
@@ -17,6 +17,22 @@ app.configure(function(){
 app.configure('development', function(){  
   app.use(express.errorHandler());  
 });  
+
+//add
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
+app.get('/', function(req, res, next) {
+  // Handle the get for this route
+});
+
+app.post('/', function(req, res, next) {
+ // Handle the post for this route
+});
+
   
 routes = require('./routes/Router')(app);  
   
